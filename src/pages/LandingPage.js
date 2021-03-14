14a03dd40f82757cba3751ebf6745de4f4db567a
irsyadfptr <75597952+irsyadfptr@users.noreply.github.com>
2021-03-14 19:07:16 +0700

    Added heroes in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Hero from '../parts/Hero'
 
 import Header from '../parts/Header'
-
+import landingPage from '../json/landingPage.json'
 export default class LandingPage extends React.Component {
     render() {
         return(
             <>
                <Header {...this.props}></Header>
+               <Hero data={landingPage.hero}/>
             </>
         )
     }
